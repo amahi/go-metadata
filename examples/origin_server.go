574f5dcb61bf8b384864863f16a8165a65af5611
Carlos Puchol <cpg@rocketmail.com>
2014-08-13 11:54:16 -0700

    add two tv api parameters to example code

--- examples/origin_server.go
+++ examples/origin_server.go
@@ -8,7 +8,7 @@ import (
 const size int = 1000000
 
 func main() {
-	Lib, err := metadata.Init(size, "metadata.db", "API-KEY")
+	Lib, err := metadata.Init(size, "metadata.db", "TMDB-API-KEY", "TVRAGE-API-KEY", "TVDB-API-KEY")
 	if err != nil {
 		fmt.Println("Error:", err)
 	} else {
